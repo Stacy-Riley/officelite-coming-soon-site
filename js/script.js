e4bc10ce99dea47b4aa0aae8e9782f5a08da8f3a
stacy_r <stacyrileyca@gmail.com>
2023-06-13 15:55:00 -0500

    Added the function for the countdown feature on the home page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Connect to the DOM:
 const entireDate = document.querySelector(".span-countdown-date");
 const timeDay = document.querySelector(".time-days");
 const timeHours = document.querySelector(".time-hours");
@@ -13,42 +14,84 @@ const monthAbbreviations = [
     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
   ];
 
-window.onload = function (){
+//Pull in the current date:
+let currentDate = new Date();
+let futureDate;
 
-const currentDate = new Date();
-const futureDate = new Date();
-futureDate.setDate(currentDate.getDate() + 30);
-//day number
-const futureDay = futureDate.getDate()
-//year number
-const futureYear = futureDate.getFullYear();
+//Function to run when window loads:
+window.onload = function() {
+  startCountdown();
+};
+
+//Where the magic happens:
+function startCountdown () {
+  //Check if user has already visited site and has a futureDate stored in localStorage
+let storedDateTime = localStorage.getItem('futureDateTime');
+
+
+if(storedDateTime){
+  //Here we want to use the storedDate. 
+  //if it is available convert the stored timestamp back to a Date object
+  futureDate = new Date(parseInt(storedDateTime));
+
+  } else {
+    //If there isn't a timestamp to be found, we will make one:
+    futureDate.setDate(currentDate.getDate() + 30);
+  }
+
+//Get the day number:
+// const futureDay = futureDate.getDate()
+const futureDay = new Date(futureDate).getDate();
+//Get the year number:
+const futureYear = new Date(futureDate).getFullYear();
 
 //To get the name of the future month:
-const futureMonthIndex = futureDate.getMonth();
+const futureMonthIndex = new Date(futureDate).getMonth();
 const futureMonthName = monthNames[futureMonthIndex];
 const futureMonthAbbreviation = monthAbbreviations[futureMonthIndex];
 
-//To put the entire date in place:
+//To put the entire date on the page:
 const formatedDate = futureDay + " " + futureMonthAbbreviation + " " + futureYear;
 entireDate.textContent = formatedDate;
 
+// Store the future date and time as a timestamp in local storage for next time the page loads:
+localStorage.setItem('futureDateTime', futureDate.getTime());
+
+
+//Countdown function to display individual numbers:
+const countdown =  setInterval(function () {
+  
+  const nowTime = new Date().getTime();
+  let timeRemaining = futureDate - nowTime;
+
+  //Break down the days, hours, minutes, and seconds remaining:
+  let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+  //Display on page: 
+  timeDay.textContent = `${days}`;
+  timeHours.textContent = `${hours}`;
+  timeMinutes.textContent = `${minutes}`;
+  timeSeconds.textContent = `${seconds}`;
+
+   // If the countdown is finished, countdown gets restarted:
+   if (timeRemaining < 0) {
+    clearInterval(countdown);
+    console.log('Countdown finished!');
+    startCountdown(); //restarts here
+  }
+
+}, 1000);
 }
 
-// console.log("Future date: " + futureDate)
-// console.log("Future Day number: " + futureDay)
-// console.log("Future Month: " + futureMonth)
-// console.log("Future Year: " + futureYear)
-// console.log("Future Month name: " + futureMonthName);
-// console.log("Future Month abbreviation: " + futureMonthAbbreviation);
 
 
-//  Steps:
-//  Bring in future date and display
-//  Set the clock to 30days into the future from original page load
-//  set clock to work backwards, count down from this date
 
-//  When the clock reaches 0, reload to another 30 days out into the future
-//
-//
-//
-//
+
+//  Steps for this program:
+//  Bring in future date and display on homepage.
+//  Set the clock to 30 days into the future from original page load.
+//  Set clock to work backwards, count down from this future date.
+//  When the clock reaches 0, reload to another 30 days out into the future.
